76efb41143720c3a819bb53537c412b1ccecbb91
k17-vp-1752031 <39826674+k17-vp-1752031@users.noreply.github.com>
2020-04-13 21:34:08 +0700

    Add files via upload

---
+++ DemoIndexOf.java
@@ -0,0 +1,12 @@
+package hcmus_1752031;
+
+public class DemoIndexOf {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str = new String("Demo method IndexOf");
+        int indexReturn = str.indexOf('I',0);
+        System.out.println("Found Index: "+indexReturn); //12
+
+    }
+}
